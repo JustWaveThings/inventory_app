eb5f0536fc8735b898d53282e7938b74cee8367c
JustWaveThings <justwavethings@proton.me>
2023-08-06 19:39:57 -0400

    Create diameter schema

---
+++ inventory_app/models/diameter.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DiameterSchema = new Schema({
+    size: {
+        type: Number, 
+        required: true,
+        enum: [1.75, 3],
+        default: 1.75
+    },
+});
+
+// virtual for diameter url
+
+DiameterSchema.virtual('url').get(function() {
+    return '/catalog/diameter/' + this._id;
+});
+
+module.exports = mongoose.model('Diameter', DiameterSchema);
