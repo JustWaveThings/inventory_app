727b53f11f14cf938b9711e77f8fae9952ca543d
JustWaveThings <justwavethings@proton.me>
2023-08-06 19:28:49 -0400

    Create brand schema

---
+++ inventory_app/models/brand.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+    name: {type: String, required: true, min: 3, max: 100},
+    description: {type: String, required: false},
+    websiteUrl: {type: String, required: false},
+
+});
+
+// virtual for url
+
+BrandSchema.virtual('url').get(function() {
+    return '/catalog/brand/' + this._id;
+});
+
+    
