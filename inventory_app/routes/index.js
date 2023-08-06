7be5652722b4923cb5eb56e0a6dca72de2b4188a
JustWaveThings <justwavethings@proton.me>
2023-08-06 11:22:07 -0400

    Set / to redirect to /catalog

--- inventory_app/routes/index.js
+++ inventory_app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'F.O.R.G.E. - Filament Outlet: Ready, GetSet, Extrude!' });
+  res.redirect("/catalog");
 });
 
 module.exports = router;
