43b385758eb02375f4045237ce0ad640aa648d60
JustWaveThings <justwavethings@proton.me>
2023-08-07 13:29:34 -0400

    Uodated description to required

--- inventory_app/models/material.js
+++ inventory_app/models/material.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MaterialSchema = new Schema({
-    name: {type: String, required: true, min: 3, max: 100},
-    description: {type: String, required: false},
+    name: {type: String, required: true, min: 3, max: 1000},
+    description: {type: String, required: true, min: 3, max: 1000},
 
 });
 
