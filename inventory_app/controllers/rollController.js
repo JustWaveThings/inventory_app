d4187f2099eb1901ef94820927450361f199d16a
JustWaveThings <justwavethings@proton.me>
2023-08-22 15:07:59 -0400

    Create roll delete get and post handlers with view

--- inventory_app/controllers/rollController.js
+++ inventory_app/controllers/rollController.js
@@ -58,84 +58,6 @@ exports.roll_create_get = asyncHandler(async (req, res, next) => {
 
 // handle roll create on POST
 
-/* exports.roll_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: roll update GET");
-}); */
-
-/* exports.roll_create_post = [
-  // handle iterating through brand, material and diameter arrays
-  (req, res, next) => {
-    if (!(req.body.brand instanceof Array)) {
-      if (typeof req.body.brand === "undefined") req.body.brand = [];
-      else req.body.brand = new Array(req.body.brand);
-    }
-    if (!(req.body.material instanceof Array)) {
-      if (typeof req.body.material === "undefined") req.body.material = [];
-      else req.body.material = new Array(req.body.material);
-    }
-    if (!(req.body.diameter instanceof Array)) {
-      if (typeof req.body.diameter === "undefined") req.body.diameter = [];
-      else req.body.diameter = new Array(req.body.diameter);
-    }
-  },
-
-  // validate and sanitize fields
-  body("brand.*").escape(),
-  body("material.*").escape(),
-  body("diameter.*").escape(),
-  body("weight", "Weight must be specified")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  body("color", "Color must be specified").trim().isLength({ min: 1 }).escape(),
-  body("price", "Price must be specified").trim().isLength({ min: 1 }).escape(),
-  body("quantity", "Quantity must be specified")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  body("SKU", "SKU must be specified").trim().isLength({ min: 1 }).escape(),
-  body("Description", "Description must be specified")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-
-  asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
-
-    const roll = new Roll({
-      brand: req.body.brand,
-      material: req.body.material,
-      diameter: req.body.diameter,
-      weight: req.body.weight,
-      color: req.body.color,
-      price: req.body.price,
-      quantity: req.body.quantity,
-      SKU: req.body.SKU,
-      Description: req.body.Description,
-    });
-
-    if (!errors.isEmpty()) {
-      const [brands, materials, diameters] = await Promise.all([
-        Brand.find({}).exec(),
-        Material.find({}).exec(),
-        Diameter.find({}).exec(),
-      ]);
-      res.render("roll_form", {
-        title: "Create Roll",
-        brands,
-        materials,
-        diameters,
-        roll: roll,
-        errors: errors.array(),
-      });
-    } else {
-      console.log("roll: " + roll._id);
-      await roll.save();
-      res.redirect(roll.url);
-    }
-  }),
-]; */
-
 exports.roll_create_post = [
   //validate and sanitize fields
   body("brand.*").escape(),
@@ -201,13 +123,31 @@ exports.roll_create_post = [
 // display roll delete form on GET
 
 exports.roll_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: roll delete GET");
+  const roll = await Roll.findById(req.params.id)
+    .populate("material brand diameter")
+    .exec();
+  res.render("roll_delete", { title: "Delete Roll", item: roll });
+  if (roll == null) {
+    res.redirect("/catalog/rolls");
+  }
+  res.render("roll_delete", { title: "Delete Roll", item: roll });
 });
 
 // handle roll delete on POST
 
 exports.roll_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: roll delete POST");
+  const roll = await Roll.findById(req.params.id)
+    .populate("material brand diameter")
+    .exec();
+
+  if (roll == null) {
+    res.redirect("/catalog/rolls");
+    return;
+  } else {
+    console.log(req.body.id);
+    await Roll.findByIdAndRemove(req.body.id);
+    res.redirect("/catalog/rolls");
+  }
 });
 
 // display roll update form on GET
